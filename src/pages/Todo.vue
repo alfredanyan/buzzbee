ba07253f4141b446ad350c893e9e71c6f772903b
aanyan <a.anyan@zoovu.com>
2021-09-10 19:11:08 +0200

    DONE

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,11 +1,37 @@
 <template>
-  <q-page class="q-pa-lg bg-grey-3">
-    <h5 class="q-mt-none">Todo</h5>
+  <q-page class="column bg-grey-3">
+    <div class="row q-pa-sm bg-primary">
+<q-input
+          filled
+          @keyup.enter="addTask"
+          square
+          bg-color="white"
+          class="col"
+          bottom-slots
+          v-model="newTask"
+          placeholder="Add Task"
+          dense>        
+
+
+    
+
+        <template v-slot:append>
+          <q-btn
+            round
+            dense
+            flat
+            @click="addTask"
+            icon="add" />
+        </template>
+      </q-input>
+    </div>
+    <!-- <h5 class="q-mt-none">Todo</h5> -->
     <q-list separator bordered class="bg-white">
       <q-item
         v-for="(task, index) in tasks"
         @click="task.done = !task.done"
         :key="task.title"
+        :class="{'done bg-blue-1' : task.done}"
         clickable
         v-ripple
       >
@@ -28,6 +54,15 @@
         </q-item-section>
       </q-item>
     </q-list>
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon 
+      name="check"
+      size="100px"
+      color="primary" />
+      <div class="text-h5 text-primary text-center">
+      No tasks
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -41,19 +76,20 @@ export default defineComponent({
   // },
   data() {
     return {
+      newTask:'',
       tasks: [
-        {
-          title: "Get Apples",
-          done: false,
-        },
-        {
-          title: "eat apples",
-          done: false,
-        },
-        {
-          title: "poop apples",
-          done: false,
-        },
+        // {
+        //   title: "Get Apples",
+        //   done: false,
+        // },
+        // {
+        //   title: "eat apples",
+        //   done: false,
+        // },
+        // {
+        //   title: "poop apples",
+        //   done: false,
+        // },
       ],
     };
   },
@@ -69,7 +105,27 @@ export default defineComponent({
         this.tasks.splice(index, 1);
         this.$q.notify('Task successfully deleted')
       })
+    },
+    addTask() {
+      this.tasks.push({
+        title:this.newTask,
+        done:false
+      });
+      this.newTask="";
+
     }
   }
 });
 </script>
+
+<style lang="scss">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+  .no-tasks {
+    opacity: 0.5;
+  }
+</style>
