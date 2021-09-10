eea37e5d6dbccaea9a3184d4606c3e4cc1c5f571
aanyan <a.anyan@zoovu.com>
2021-09-10 18:22:44 +0200

    confirm delete

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -3,21 +3,28 @@
     <h5 class="q-mt-none">Todo</h5>
     <q-list separator bordered class="bg-white">
       <q-item
-        v-for="task in tasks"
+        v-for="(task, index) in tasks"
         @click="task.done = !task.done"
         :key="task.title"
         clickable
         v-ripple
       >
         <q-item-section avatar>
-<q-checkbox
+          <q-checkbox
             class="no-pointer-events"
             v-model="task.done"
-            color="primary" />
-
+            color="primary"
+          />
         </q-item-section>
         <q-item-section>
           <q-item-label> {{ task.title }} </q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+          @click.stop = "deleteTask(index)"
+            round
+            color="red"
+            icon="delete" />
         </q-item-section>
       </q-item>
     </q-list>
@@ -25,9 +32,13 @@
 </template>
 
 <script>
+// import { useQuasar } from "quasar";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  // setup() {
+  //   const $q = useQuasar()
+  // },
   data() {
     return {
       tasks: [
@@ -46,5 +57,19 @@ export default defineComponent({
       ],
     };
   },
+  methods: {
+    deleteTask(index) {
+       this.$q.dialog({
+        title: 'Confirm',
+        message: 'Really want to delete task?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        // console.log('>>>> OK')
+        this.tasks.splice(index, 1);
+        this.$q.notify('Task successfully deleted')
+      })
+    }
+  }
 });
 </script>
